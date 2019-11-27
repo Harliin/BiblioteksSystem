49ae37d5f5fc28412ce2e819862f678c40851023
Harliin <56713048+Harliin@users.noreply.github.com>
2019-11-27 10:08:42 +0100

    Grund Programmet

---
+++ BiblioteksSystem/BiblioteksSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BiblioteksSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
