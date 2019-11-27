d852f5b0d5aa95ecd9f3d04a0a57e61813274454
Pontuspe <pontus-pettersson@hotmail.com>
2019-11-27 10:56:41 +0100

    MenyAlphaVersion

---
+++ BibliotekSystem/BibliotekSystem/BookClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BibliotekSystem
+{
+    class BookClass
+    {
+        public string title { get; set; }
+        public string author { get; set; }
+
+        public BookClass(string title, string author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+    }
+}
